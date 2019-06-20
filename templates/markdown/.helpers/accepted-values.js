792f632f632fcccb243832a5cd3531faf296ab4f
Damien Pitard <d.pitard@creads.org>
2019-06-20 10:28:34 +0200

    Add jstransformer to support any templating engine

--- templates/markdown/.helpers/accepted-values.js
+++
@@ -1,9 +0,0 @@
-module.exports = (Handlebars, _) =>{
-
-  Handlebars.registerHelper('acceptedValues', items =>{
-    if(!items) return '<em>Any</em>';
-
-    return items.map(i => `<code>${i}</code>`).join(', ');
-  });
-
-}
